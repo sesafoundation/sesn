91f3b910c31c5796254feca5a4770471278daf76
Sesa Foundation <rohan@sesa.foundation>
2025-11-07 21:17:53 +0530

    update

--- consensus/sonium/abi.go
+++ consensus/sonium/abi.go
@@ -522,6 +522,19 @@ const validatorABI = `[
     },
     {
       "inputs": [],
+      "name": "SlashProtect",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
       "name": "StakingLockPeriod",
       "outputs": [
         {
