558ce0dcca4c835a509bc4688c97ff3e83ee4079
Sesa Foundation <rohan@sesa.foundation>
2025-11-16 18:58:56 +0530

    u

--- cmd/preconf-builder/main.go
+++ cmd/preconf-builder/main.go
@@ -34,8 +34,8 @@ func main() {
 	  	IPCPath  : "/node1/setd.ipc",
         WSListen:   ":8556",
         HTTPListen: ":8557",
-        KeystorePath  = "/node1/keystore/0xf3a42f681d6070afd77cd206e79ef6dbdf5604eb.json",
-        KeyPassword   = "/node1/key.txt",
+        KeystorePath : "/node1/keystore/0xf3a42f681d6070afd77cd206e79ef6dbdf5604eb.json",
+        KeyPassword  : "/node1/key.txt",
         
     }
 
