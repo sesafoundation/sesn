220a5cac6d7f5649e3ebd49f5b810d5983049035
Sesa Foundation <rohan@sesa.foundation>
2025-11-20 14:37:09 +0530

    u

--- preconf/client.go
+++ preconf/client.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/sesafoundation/sesn/common"
+	//"github.com/sesafoundation/sesn/common"
 )
 
 // PreconfClient holds the latest miniblock streamed by the builder
