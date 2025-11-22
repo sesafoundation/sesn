6df78a73f5a8f4ec02efe30a60a189f596983549
Sesa Foundation <rohan@sesa.foundation>
2025-11-22 18:31:26 +0530

    update

--- internal/preconfapi/preconf_api.go
+++ internal/preconfapi/preconf_api.go
@@ -5,25 +5,25 @@ import (
 
     "github.com/sesafoundation/sesn/common"
     "github.com/sesafoundation/sesn/preconf"
-	//"github.com/sesafoundation/sesn/internal/ethapi"
 )
 
-type PublicPreconfAPI struct {
-    //backend *ethapi.EthAPIBackend
-	backend, ok := api.backend.(*ethapi.EthAPIBackend)
+// This interface is implemented by EthAPIBackend (added in backend.go)
+type PreconfBackend interface {
+    GetPreconfReceipt(hash common.Hash) (*preconf.PreconfReceipt, bool)
 }
 
-func NewPublicPreconfAPI(b *ethapi.EthAPIBackend) *PublicPreconfAPI {
+type PublicPreconfAPI struct {
+    backend PreconfBackend
+}
+
+func NewPublicPreconfAPI(b PreconfBackend) *PublicPreconfAPI {
     return &PublicPreconfAPI{backend: b}
 }
+
 func (api *PublicPreconfAPI) GetPreconfReceipt(ctx context.Context, txHash common.Hash) (*preconf.PreconfReceipt, error) {
     if api.backend == nil {
         return nil, nil
     }
-
-    api.backend.PreconfMu.RLock()
-    r := api.backend.PreconfReceipts[txHash]
-    api.backend.PreconfMu.RUnlock()
-
+    r, _ := api.backend.GetPreconfReceipt(txHash)
     return r, nil
 }
