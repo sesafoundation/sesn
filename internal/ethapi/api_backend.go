b44e7d360a03cd0e967a20d0ae95707a5c44336d
Sesa Foundation <rohan@sesa.foundation>
2025-11-29 23:33:23 +0530

    update

--- internal/ethapi/api_backend.go
+++ internal/ethapi/api_backend.go
@@ -247,12 +247,8 @@ func (b *EthAPIBackend) ExtRPCEnabled() bool {
     return b.backend.ExtRPCEnabled()
 }
 
-func (b *EthAPIBackend) GetEVM(
-    msg core.Message,
-    header *types.Header,
-    statedb *state.StateDB,
-    cfg vm.Config,
-) (*vm.EVM, error) {
+func (b *EthAPIBackend) GetEVM(msg core.Message, header *types.Header, state *state.StateDB, cfg vm.Config) (*vm.EVM, error)
+ {
 
     // delegate to backend (Ethereum)
     return b.backend.GetEVM(msg, header, statedb, cfg)
