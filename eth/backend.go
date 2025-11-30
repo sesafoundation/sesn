ca5f4ace7f4dcf0384adb695a08d6b8f96c0ec94
Sesa Foundation <rohan@sesa.foundation>
2025-11-30 10:58:30 +0530

    update

--- eth/backend.go
+++ eth/backend.go
@@ -678,6 +678,13 @@ func (s *Ethereum) ExtRPCEnabled() bool {
 
 // GetEVM implements ethapi.Backend. It returns an EVM instance with the
 // given block and state, used by the debug/tracer APIs.
+func (s *Ethereum) GetEVM(msg core.Message, header *types.Header, statedb *state.StateDB, cfg vm.Config) (*vm.EVM, error) {
+    // Prepare block and tx context
+    blockCtx := core.NewEVMBlockContext(header, s.blockchain, nil)
+    txCtx := core.NewEVMTxContext(msg)
+
+    return vm.NewEVM(blockCtx, txCtx, statedb, s.blockchain.Config(), cfg), nil
+}
 
 
 func (eth *Ethereum) GetEVM(
@@ -701,3 +708,4 @@ func (eth *Ethereum) GetEVM(
 
     return evm, vmErr, nil
 }
+
